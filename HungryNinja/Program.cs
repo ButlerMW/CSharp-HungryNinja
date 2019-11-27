6a1129693b513d2e07fad1b9693f8ade3e158e87
ButlerMW <57372823+ButlerMW@users.noreply.github.com>
2019-11-27 13:59:16 -0700

    Add files via upload

---
+++ HungryNinja/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace HungryNinja
+{
+    class Food
+    {
+        public string Name;
+        public int Calories;
+        // Foods can be Spocr and or sweet
+        public bool IsSpicy;
+        public bool IsSweet;
+        // DONE:add a constructor that takes in all four parameters: Name, Calories, IsSpicy, IsSweet
+        public Food(string name, int calories, bool spicy, bool sweet)
+        {
+            Name = name;
+            Calories = calories;
+            IsSpicy = spicy;
+            IsSweet = sweet;
+        }
+    }
+    class Buffet
+    {
+        public List<Food> Menu;
+
+        // constructor
+        public Buffet()
+        {
+            Menu = new List<Food>()
+            {
+                new Food("Curry", 500, true, false)
+                new Food("Sushi", 250, false, false)
+                new Food("Gyro", 700, true, false)
+                new Food("Mystery Meat", 100, false, false)
+                new Food("Steak n Taters", 800, false, false)
+                new Food("Ice Cream", 500, false, true)
+                new Food("BBQ", 1000, true, true)
+                
+            };
+        }
+
+        public Food Serve()
+        {
+            Random rand = new Random();
+            
+        }
+    }
+
+    class HungryNinja
+    {
+        private int calorieIntake;
+        public List<Food> FoodHistory;
+
+        // add a constructor
+
+        // add a public "getter" property called "ItsFull"
+
+        // build out the Eat method
+        public void Eat(Food item)
+        {
+
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    }
+}
